eebe84a429bb0a8738548d2cc982953ed6707825
sakellarios zairis <szairis@gmail.com>
2016-03-17 03:47:47 -0400

    minimal testing indicates the the package is working; gitignore modified for setup.py build

--- sakmapper/lens.py
+++ sakmapper/lens.py
@@ -1,7 +1,17 @@
-def apply_lens(arr, lens='mds', n_dim=2):
+import pandas as pd
+from sklearn import manifold
+
+def apply_lens(df, lens='mds', n_dim=2):
     '''
-    input: N x F matrix of observations
+    input: N x F dataframe of observations
     output: N x n_dim image of input data under lens function
     '''
     
-    return arr_lens
+    if lens != 'mds':
+        raise 'error: only MDS lens is currently supported'
+    if n_dim > 2 or n_dim < 1:
+        raise 'error: image of data set must be dimension 1 or 2'
+    
+
+    df_lens = pd.DataFrame(manifold.MDS(n_components=n_dim).fit_transform(df), df.index)
+    return df_lens
