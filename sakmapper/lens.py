6975d331c0a257d10b2fb644b16610fde0e62ce2
sakellarios zairis <szairis@gmail.com>
2016-11-10 15:32:59 -0500

    introducing choices for lens distance metric other than euclidean

--- sakmapper/lens.py
+++ sakmapper/lens.py
@@ -1,21 +1,25 @@
 import pandas as pd
-from sklearn import decomposition, manifold
+from sklearn import decomposition, manifold, metrics
 
 
-def apply_lens(df, lens='pca', n_dim=2):
+def apply_lens(df, lens='pca', dist='euclidean', n_dim=2):
     '''
     input: N x F dataframe of observations
     output: N x n_dim image of input data under lens function
     '''
     if n_dim != 2:
         raise 'error: image of data set must be two-dimensional'
+    if lens == 'pca' and dist != 'euclidean':
+        raise 'error: PCA requires the use of euclidean distance metric'
+
+    D = metrics.pairwise.pairwise_distances(df, metric=dist)
 
     if lens == 'pca':
         df_lens = pd.DataFrame(decomposition.PCA(n_components=n_dim).fit_transform(df), df.index)
     elif lens == 'mds':
-        df_lens = pd.DataFrame(manifold.MDS(n_components=n_dim).fit_transform(df), df.index)
+        df_lens = pd.DataFrame(manifold.MDS(n_components=n_dim).fit_transform(D), df.index)
     elif lens == 'neighbor':
-        df_lens = pd.DataFrame(manifold.SpectralEmbedding(n_components=n_dim).fit_transform(df), df.index)
+        df_lens = pd.DataFrame(manifold.SpectralEmbedding(n_components=n_dim).fit_transform(D), df.index)
     else:
         raise 'error: only PCA, MDS, neighborhood lenses are currently supported'
     
